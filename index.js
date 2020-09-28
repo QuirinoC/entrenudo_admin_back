4120b70d9495dbc820f6fdff2bffec86aaa97d79
Juan Carlos Quirino <juanquirinoc@gmail.com>
2020-09-27 18:28:33 -0600

    Added form for new orders

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+let http = require('http')
+let fs = require('fs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
 
 // DB Connection
 // TODO: Replace with production string using env variables
 var mongoose = require('mongoose');
 const { Schema } = mongoose;
 //const DB_URL = "mongodb+srv://mongodb-stitch-something-suzft:<password>@cluster0-bspcz.mongodb.net/entrenudo_admin?retryWrites=true&w=majority";
-const uri = "mongodb+srv://admin:admin123@entrenudo.bspcz.mongodb.net/entrenudo_admin?retryWrites=true&w=majority"
+const uri = "mongodb://admin:admin123@ds127936.mlab.com:27936/entrenudo"
 const LOCAL = 'mongodb://localhost:27017/entrenudo_admin'
 
 mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverSelectionTimeoutMS: 5000
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000
 }).catch(err => console.log(err.reason));
 
 
@@ -34,23 +37,28 @@ var port = process.env.PORT || 8080;
 
 var router = express.Router();
 
-router.get('/', function (req, res) {
+router.get('/', function(req, res) {
     res.json({ message: 'Entrenudo Orders Admin' });
 });
 
-router.get('/order', async (req, res) => {
+router.get('/order', async(req, res) => {
     let page = req.query.page || 0;
     let per_page = req.query.per_page || 5;
     const orders = await Order.find({})
     res.send(orders);
 })
-router.post('/order', async (req, res) => {
+router.post('/order', async(req, res) => {
     console.log(req.body)
     const order = new Order(
         req.body
     );
-    order.save();
-    res.send(order);
+    await order.save();
+    res.redirect('https://google.com');
+})
+
+router.get('/details', async(req, res) => {
+    res.writeHead(200, { 'content-type': 'text/html' })
+    fs.createReadStream('form.html').pipe(res)
 })
 
 app.use('/api', router);

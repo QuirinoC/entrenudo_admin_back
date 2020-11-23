b11a3f0899ed723e2c6a6f05c064a2bb55922d4f
Juan Carlos Quirino <juanquirinoc@gmail.com>
2020-11-23 09:52:18 -0700

    Added mock test as placeholders

---
+++ tests/api.test.js
@@ -0,0 +1,19 @@
+const axios = require('axios');
+const URL = process.env.url | 'localhost:8080/api'
+
+
+test("Get orders for DB", () => {
+  return Promise.resolve(() => {});
+});
+
+test("Store post to DB", () => {
+  return Promise.resolve(() => {});
+});
+
+test("Store image to AWS", () => {
+  return Promise.resolve(() => {});
+});
+
+test("Connect to DB", () => {
+  return Promise.resolve(() => {});
+});
